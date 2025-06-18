d442ef39dc9fb0242063bf0cfca71e8d7f899ffe
Phantom <ramonadedotun550@gmail.com>
2025-06-19 00:50:32 +0100

    The Navigation and Top content Very hard

---
+++ headphones/script.js
@@ -0,0 +1,9 @@
+function OpenNavbar() {
+    document.getElementById("nav-list").style.display = "flex";
+    document.getElementById("Open-navbar").style.display = "none";
+  }
+  
+  function CloseNavbar() {
+    document.getElementById("nav-list").style.display = "none";
+    document.getElementById("Open-navbar").style.display = "flex";
+  }
